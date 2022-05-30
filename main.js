402f9ebbda351bd832ffdce2fcc3a6cb3b7123be
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-05-30 21:29:12 +0200

    added responsiveness

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const toggleMenu = document.querySelector(".menu");
+const navItem = document.querySelector(".nav__lists");
+const navbar = document.querySelector(".header");
+
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("active");
+    navItem.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav__list-link").forEach(e => 
+    e.addEventListener("click", () => {
+        toggleMenu.classList.remove("active");
+        navItem.classList.remove("active");
+}));
+
 window.addEventListener("scroll", () => {
-    console.log("valami")
-    var navbar = document.querySelector(".header");
     navbar.classList.toggle("nav-srolled", window.scrollY > 0);
 })
+
